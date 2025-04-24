567e36451d6d5203ad57d6f40186d24ac6ad1838
minhngoc28 <148454550+minhngoc28@users.noreply.github.com>
2025-04-24 02:11:29 +0200

    Delete init.sql

--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS swift_codes (
-    id SERIAL PRIMARY KEY,
-    swift_code VARCHAR(11) UNIQUE NOT NULL,
-    bank_name VARCHAR(255) NOT NULL,
-    address TEXT NOT NULL,
-    country_iso2 CHAR(2) NOT NULL,
-    country_name VARCHAR(255) NOT NULL,
-    is_headquarter BOOLEAN NOT NULL
-);

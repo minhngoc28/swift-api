92d126a3ae2065f96e996e42b1bc80402d7526a5
minhngoc28 <148454550+minhngoc28@users.noreply.github.com>
2025-04-24 02:10:53 +0200

    init.sql

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS swift_codes (
     id SERIAL PRIMARY KEY,
-    swift_code TEXT UNIQUE NOT NULL,
-    bank_name TEXT NOT NULL,
+    swift_code VARCHAR(11) UNIQUE NOT NULL,
+    bank_name VARCHAR(255) NOT NULL,
     address TEXT NOT NULL,
-    country_iso2 TEXT NOT NULL,
-    country_name TEXT NOT NULL,
+    country_iso2 CHAR(2) NOT NULL,
+    country_name VARCHAR(255) NOT NULL,
     is_headquarter BOOLEAN NOT NULL
 );
